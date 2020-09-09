7ecc30439b396d5f50f9014b0b6b3c25a0ca0de9
neha <nehamadaram156@gmail.com>
2020-09-09 15:00:44 -0400

    done

--- src/components/recentOrders.js
+++ src/components/recentOrders.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/card'
 const RecentOrder = ()=> {
     return (
         <div>
