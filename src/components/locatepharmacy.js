99f040cb00d78193456697e3f38077d239d32452
neha <nehamadaram156@gmail.com>
2020-09-09 15:03:24 -0400

    done

--- src/components/locatepharmacy.js
+++ src/components/locatepharmacy.js
@@ -2,8 +2,6 @@ import React from 'react'
 import './components.css'
 import MapView from './mapview'
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/card'
 const Home = () => {
     const stylePrescriptions = {backgroundColor : 'red'};
     const styleOrders = {backgroundColor : 'blue'};
